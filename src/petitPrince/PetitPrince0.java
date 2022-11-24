37520c7cefee5caa1a68e3c8f6e77e2150b4b172
moisesfil <moisesfil@172.17.155.11>
2022-11-24 15:30:12 +0100

    v1

---
+++ src/petitPrince/PetitPrince0.java
@@ -0,0 +1,25 @@
+package petitPrince;
+
+import asteroide.Sujet;
+
+public class PetitPrince0 implements Sujet {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getArgent() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
